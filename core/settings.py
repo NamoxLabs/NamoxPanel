b19c66a2d0ef2bad97fb3a1f0eaf3104dd639cc2
Giovanni <giovanniyatze@gmail.com>
2018-11-02 03:22:47 -0600

    Core settings modified

--- core/settings.py
+++ core/settings.py
@@ -82,15 +82,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
-
 if os.getenv('DOCKER_CONTAINER'):
     POSTGRES_HOST = 'db'
 else:
@@ -106,7 +97,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
