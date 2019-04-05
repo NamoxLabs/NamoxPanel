4d87c2fcf25653a9489081476c279c1fa631e532
Giovanni <giovanniyatze@gmail.com>
2019-04-05 16:48:56 -0600

    reload added in docker compose, url redirect fixed in template and lib pending imported

--- namoxpanel/apps/views.py
+++ namoxpanel/apps/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 from .forms import AppForm
 
@@ -14,16 +15,16 @@ def app_review(request):
 
 
 def create_app(request):
-    app_form = AppForm(data=request.POST or None)
+    app_form=AppForm(data=request.POST or None)
     if app_form.is_valid():
         app_form.save()
     else:
         # if a GET (or any other method) we'll create a blank form
         app_form = AppForm()
+    ctx={'app_form': app_form}
 
-    ctx = {'app_form': app_form}
-    return TemplateResponse(request, 'app/create_app.html', ctx)
-    # return render(request, 'app/create_app.html', context=ctx)
+    # return TemplateResponse(request, 'app/create_app.html', ctx)
+    return render(request, 'app/create_app.html', context=ctx)
 
 
 def delete_app(request):
